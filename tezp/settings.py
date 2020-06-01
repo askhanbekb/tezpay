e84a4b2cca9a5ac125d2e4b7890cd9261b9e9639
Beka <askhanbekb@gmail.com>
2020-06-01 10:07:54 +0500

    DB UPDATE 2

--- tezp/settings.py
+++ tezp/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'tezp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd2jm76pa6vrrlp',
+        'USER': 'bqbxherpvwspam',
+        'PASSWORD': '140ab09d6b54ee362f26fd69cb4c52d8eb40c987cc2b8a6adc0ff5dc1d50f5ae',
+        'HOST': 'ec2-54-75-246-118.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
